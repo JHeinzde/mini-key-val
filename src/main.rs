6f4eb666094422df3219a45ac14395f2abbf650e
jonathan <Jonathan.Heinz@t-systems.com>
2021-03-28 21:27:23 +0200

    Implementing an append only log.
    Takes enums of type Action, which will
    be used for Raft operations. Probably needs
    to be extended to contain election actions.

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,9 @@ mod raft;
 fn main() {
     let storage: HashMap<String, Vec<u8>> = HashMap::new();
     let cache = Cache(storage);
-    rocket::ignite().mount("/", routes![handlers::get_cache_value, handlers::insert_into_cache])
+    rocket::ignite().mount("/", routes![handlers::get_cache_value,
+     handlers::insert_into_cache,
+     handlers::delete_cache_value])
         .manage(Mutex::new(cache))
         .launch();
 }
