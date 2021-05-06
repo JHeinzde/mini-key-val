b46c667abe3fb28ffa12736fa0adf3fdba47c69b
Jonathan Heinz <20475437+jheinzde@users.noreply.github.com>
2021-05-07 00:17:51 +0200

    Starting to implement a rpc endpoint

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,13 +4,15 @@ use std::io::Read;
 use std::sync::Mutex;
 
 use rocket::{Data, State};
-use rocket::response::status::NotFound;
+use rocket::response::status::{NotFound, BadRequest};
+use crate::raft::{RaftState, AppendEntry};
+use rocket_contrib::json::Json;
 
 pub struct Cache(pub HashMap<String, Vec<u8>>);
 
 
 #[post("/insert/<uuid>", data = "<object>")]
-pub fn insert_into_cache(uuid: String, object: Data, storage: State<Mutex<Cache>>) -> Result<(), Box<dyn Error>> {
+pub fn insert_into_cache(uuid: String, object: Data, storage: State<Mutex<Cache>>, raft: State<Mutex<RaftState>>) -> Result<(), Box<dyn Error>> {
     let mut body_stream = object.open();
     let mut value = Vec::new();
     body_stream.read_to_end(&mut value)?;
@@ -22,7 +24,7 @@ pub fn insert_into_cache(uuid: String, object: Data, storage: State<Mutex<Cache>
 }
 
 #[get("/retrieve/<uuid>")]
-pub fn get_cache_value(uuid: String, storage: State<Mutex<Cache>>) -> Result<Vec<u8>, NotFound<String>> {
+pub fn get_cache_value(uuid: String, storage: State<Mutex<Cache>>, raft: State<Mutex<RaftState>>) -> Result<Vec<u8>, NotFound<String>> {
     let cache = storage.lock().unwrap();
     if cache.0.contains_key(&uuid) {
         Ok(cache.0.get(&uuid).unwrap().clone())
@@ -32,7 +34,7 @@ pub fn get_cache_value(uuid: String, storage: State<Mutex<Cache>>) -> Result<Vec
 }
 
 #[delete("/delete/<uuid>")]
-pub fn delete_cache_value(uuid: String, storage: State<Mutex<Cache>>) -> Result<(), NotFound<String>> {
+pub fn delete_cache_value(uuid: String, storage: State<Mutex<Cache>>, raft: State<Mutex<RaftState>>) -> Result<(), NotFound<String>> {
     let mut cache = storage.lock().unwrap();
     if cache.0.contains_key(&uuid) {
         cache.0.remove(&uuid);
@@ -40,6 +42,18 @@ pub fn delete_cache_value(uuid: String, storage: State<Mutex<Cache>>) -> Result<
     } else {
         Err(NotFound(String::from("This key was not found")))
     }
+}
+
+#[post("/append_entries", data = "<entry>")]
+pub fn append_entries(entry: Json<AppendEntry>, raft: State<Mutex<RaftState>>) -> Result<(), BadRequest<String>> {
+    let mut raft_state = raft.lock().unwrap();
+
+    if entry.0.term < raft_state.current_term {
+        return Err(BadRequest(Option::from(String::from("Term sent is smaller than current_term"))))
+    }
+
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -56,7 +70,7 @@ mod test {
         let rocket = rocket::ignite().mount("/", routes![get_cache_value,
             insert_into_cache,
             delete_cache_value])
-                   .manage(Mutex::new(cache));
+            .manage(Mutex::new(cache));
         let client = Client::new(rocket).expect("valid rocket instance");
 
         let key = String::from("this-is-a-test-key");
--- src/raft.rs
+++ src/raft.rs
@@ -1,12 +1,46 @@
 use crate::raft::Action::Heartbeat;
+use serde::{Serialize, Deserialize};
 
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Log(Vec<Action>);
 
-#[derive(Debug)]
+#[derive(Serialize, Deserialize, Debug)]
 pub enum Action {
     Heartbeat,
-    SetAction { key: String, value: Vec<u8> },
-    DeleteAction { key: String },
+    SetAction { key: String, value: Vec<u8>, term: u32 },
+    DeleteAction { key: String, term: u32 },
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct RaftState {
+    pub current_term: u32,
+    pub voted_for: u32,
+    pub log: Log,
+    pub volatile_state: VolatileState,
+    pub leader_state: LeaderState,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct VolatileState {
+    pub commit_index: u64,
+    pub last_applied: u64,
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct LeaderState {
+    pub next_index: Vec<u64>,
+    pub match_index: Vec<u64>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AppendEntry {
+    pub term: u32,
+    pub leader_id: u32,
+    pub prev_log_index: u64,
+    pub prev_log_term: u32,
+    pub entries: Vec<Action>,
+    pub leader_commit: u64,
 }
 
 impl Log {
@@ -35,12 +69,12 @@ impl PartialEq for Action {
                 Action::Heartbeat => true,
                 _ => false
             }
-            Action::SetAction { value: val, key: ke } => match other {
-                Action::SetAction { value, key } => value == val && key == ke,
+            Action::SetAction { value: val, key: ke, term: te } => match other {
+                Action::SetAction { value, key, term } => value == val && key == ke && term == te,
                 _ => false
             }
-            Action::DeleteAction { key: ke } => match other {
-                Action::DeleteAction { key } => key == ke,
+            Action::DeleteAction { key: ke, term: te } => match other {
+                Action::DeleteAction { key, term } => key == ke && term == te,
                 _ => false
             }
         }
@@ -56,8 +90,9 @@ impl Clone for Action {
     fn clone(&self) -> Self {
         match self {
             Action::Heartbeat => Heartbeat,
-            Action::SetAction { value, key } => Action::SetAction { key: key.clone(), value: value.clone() },
-            Action::DeleteAction { key } => Action::DeleteAction { key: key.clone() }
+            Action::SetAction { value, key, term } =>
+                Action::SetAction { key: key.clone(), value: value.clone(), term: term.clone() },
+            Action::DeleteAction { key, term } => Action::DeleteAction { key: key.clone(), term: term.clone() }
         }
     }
 }
@@ -68,7 +103,7 @@ mod test {
 
     #[test]
     fn test_log_append() {
-        let set_action = Action::SetAction { key: String::from("test"), value: Vec::new() };
+        let set_action = Action::SetAction { key: String::from("test"), value: Vec::new(), term: 32 };
         let mut log_under_test = Log(Vec::new());
 
         log_under_test.append(set_action.clone());
